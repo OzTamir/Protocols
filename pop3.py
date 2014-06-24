45276f504cf97598d5824e3ba98275a4c51b5c2b
OzTamir <TheOzTamir@gmail.com>
2014-06-24 13:51:46 +0300

    Added some documention to pop3.py

--- pop3.py
+++ pop3.py
@@ -16,6 +16,7 @@ class EmailClient:
 		self.login(sock, user, pwd, debug)
 
 	def get_socket(self, SSL_required=True):
+		''' Create a socket (SSL enabled by default) '''
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.connect((self.server, self.port))
 		if SSL_required:
@@ -24,9 +25,11 @@ class EmailClient:
 		return sock
 
 	def read(self):
+		''' Read data from the email server '''
 		data = ''
 		while True:
 			next = self.sock.recv(1)
+			# Read data until a newline is reached
 			if next == '\n':
 				break
 			data += next
@@ -35,24 +38,32 @@ class EmailClient:
 		return data
 
 	def send(self, cmd, data='', read_at_end=True):
+		''' Send a POP3 command to the email server '''
+		# Compile the message that will be sent to the server
 		if data == '':
 			packet = '%(CMD)s\r\n' % {'CMD' : cmd.upper}
 		else:
 			packet = '%(CMD)s %(DATA)s\r\n'	% {'CMD' : cmd.upper(), 'DATA' : str(data)}
 		self.sock.send(packet)
+		# We want this to happen to avoid messages getting 'stuck in the pipe'
 		if read_at_end:
 			self.read()
 
 	def login(self):
+		''' Login to the server '''
+		# Read the session header (the server connection confirmation)
 		self.read()
+		# Send the username and password (login)
 		self.send('USER', self.username)
 		self.send('PASS', self.password)
 		return
 
 	def quit(self):
+		''' End the session '''
 		self.send('QUIT')
 
 	def get_mail(self):
+		''' Fetch the available mails waiting to be read '''
 		sock.send('STAT')
 		data = read(sock).split(' ')
 		print 'There are %s mails in your inbox.' % data[1]
